53996d05d3838516cd09be0365dcb156293a0bd7
John de la Garza <jdelagarza@cylance.com>
2020-08-20 09:12:18 -0700

    add fizz_buzz_2 implementation
    This aims to be more python specific.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -12,6 +12,14 @@ def fizz_buzz(n):
     return out
 
 
-for n in range(1, 101):
-    print(fizz_buzz(n), end=" ")
-print()
+def fizz_buzz_2(n):
+    fizz = "" if n % 3 else "fizz"
+    buzz = "" if n % 5 else "buzz"
+    return f"{fizz}{buzz}" or str(n)
+
+
+for f in (fizz_buzz, fizz_buzz_2):
+    for n in range(1, 101):
+        assert fizz_buzz(n) == fizz_buzz_2(n)
+        print(f(n), end=" ")
+    print()
