2dc009e56cc53445430508262e50dc459fa25436
John de la Garza <jdelagarza@cylance.com>
2021-02-19 11:26:06 -0800

    refactor

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-def fizz_buzz(n):
+def fizz_buzz1(n):
     out = ""
     if n % 3 == 0:
         out += "fizz"
@@ -12,14 +12,14 @@ def fizz_buzz(n):
     return out
 
 
-def fizz_buzz_2(n):
+def fizz_buzz2(n):
     fizz = "" if n % 3 else "fizz"
     buzz = "" if n % 5 else "buzz"
     return f"{fizz}{buzz}" or str(n)
 
 
-for f in (fizz_buzz, fizz_buzz_2):
-    for n in range(1, 101):
-        assert fizz_buzz(n) == fizz_buzz_2(n)
-        print(f(n), end=" ")
-    print()
+def fizz_buzz(f):
+    return ' '.join(f(n) for n in range(1, 101))
+
+if __name__ == "__main__":
+    print(fizz_buzz(fizz_buzz2))
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,23 +1,30 @@
-from fizzbuzz import fizz_buzz, fizz_buzz_2
+import pytest
+
+from fizzbuzz import fizz_buzz, fizz_buzz1, fizz_buzz2
 
 
-arg_to_expected = [
+args_to_expected = [
     (1, "1"),
+    (2, "2"),
     (3, "fizz"),
     (4, "4"),
     (5, "buzz"),
     (6, "fizz"),
+    (7, "7"),
     (10, "buzz"),
     (15, "fizzbuzz"),
     (30, "fizzbuzz"),
 ]
 
+@pytest.mark.parametrize("arg, expected", args_to_expected)
+def test_fizz_buzz1(arg, expected):
+    assert fizz_buzz1(arg) == expected
 
-def test_fizz_buzz():
-    for arg, expected in arg_to_expected:
-        assert fizz_buzz(arg) == expected
+@pytest.mark.parametrize("arg,expected", args_to_expected)
+def test_fizz_buzz2(arg, expected):
+    assert fizz_buzz2(arg) == expected
 
-
-def test_fizz_buzz_2():
-    for arg, expected in arg_to_expected:
-        assert fizz_buzz_2(arg) == expected
+@pytest.mark.parametrize("f", [fizz_buzz1, fizz_buzz2])
+def test_fizz_buzz(f):
+    expected = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz fizz 22 23 fizz buzz 26 fizz 28 29 fizzbuzz 31 32 fizz 34 buzz fizz 37 38 fizz buzz 41 fizz 43 44 fizzbuzz 46 47 fizz 49 buzz fizz 52 53 fizz buzz 56 fizz 58 59 fizzbuzz 61 62 fizz 64 buzz fizz 67 68 fizz buzz 71 fizz 73 74 fizzbuzz 76 77 fizz 79 buzz fizz 82 83 fizz buzz 86 fizz 88 89 fizzbuzz 91 92 fizz 94 buzz fizz 97 98 fizz buzz"
+    assert fizz_buzz(f) == expected
