4ea592ffcbf837270dd6eb732d66ad2f028d5044
John de la Garza <jdelagarza@cylance.com>
2023-06-16 07:08:15 -0700

    permute: add iterative implementation

--- permute.py
+++ permute.py
@@ -1,157 +1,26 @@
 #!/usr/bin/env python3.9
 
-import itertools
+
+def swap(seq, i, j):
+    seq[i], seq[j] = seq[j], seq[i]
 
 
-def ref_perms(syms):
-    return [
-        "".join(str(p) for p in ps)
-        for ps in itertools.permutations(list(syms))
-    ]
+def perm(seq, i, n):
+    # import pdb; pdb.set_trace()
+    if i >= n:
+        print(seq)
+    else:
+        for j in range(i, n):
+            swap(seq, j, i)
+            perm(seq, i + 1, n)
+            swap(seq, j, i)
 
 
-def count(to, syms):
-    def inc(n):
-        lsd = n[-1]
-        rest = n[0:-1]
-        if rest == "":
-            rest = syms[0]
-        if lsd == syms[-1]:
-            return inc(rest) + syms[0]
-        else:
-            if rest == syms[0]:
-                rest = ""
-            return rest + syms[syms.index(lsd) + 1]
+l = [0, 1]
+swap(l, 0, 1)
+assert l == [1, 0]
+swap(l, 0, 1)
+assert l == [0, 1]
 
-    n = syms[0]
-    for _ in range(to):
-        yield n.zfill(len(syms))
-        n = inc(n)
-
-
-def all_but(seq, i):
-    return seq[:i] + seq[i + 1 :]
-
-
-def flatten(l):
-    flat = []
-    for i in l:
-        if type(i) == int:
-            flat.append(i)
-            continue
-        for j in i:
-            flat.append(j)
-
-    return flat
-
-
-def perms(xs):
-    if len(xs) == 0:
-        return [[]]
-    if len(xs) == 1:
-        return [xs]
-    ps = []
-    for i, x in enumerate(xs):
-        ps.append(flatten([x] + perms(all_but(xs, i))))
-    return [ps]
-
-
-def count_perms(syms):
-    return [
-        n
-        for n in count(len(syms) ** len(syms), syms)
-        if len(n) == len(syms) and len(set(n)) == len(n)
-    ]
-
-
-syms = []
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = [0]
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = [0, 1]
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-
-def fix(ps):
-    return flatten([(flatten([xs[0], ys])) for ys in xs[1:]] for xs in ps[0])
-
-
-def all_but(seq, i):
-    return seq[:i] + seq[i + 1 :]
-
-
-def flatten(l):
-    flat = []
-    for i in l:
-        if type(i) == int:
-            flat.append(i)
-            continue
-        for j in i:
-            flat.append(j)
-
-    return flat
-
-
-def perms(xs):
-    if len(xs) == 0:
-        return [[]]
-    if len(xs) == 1:
-        return [xs]
-    ps = []
-    for i, x in enumerate(xs):
-        ps.append(flatten([x] + perms(all_but(xs, i))))
-    return [ps]
-
-
-syms = []
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = [0]
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = [0, 1]
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-
-def fix(ps):
-    return flatten([(flatten([xs[0], ys])) for ys in xs[1:]] for xs in ps[0])
-
-
-syms = [0, 1, 2]
-print("exp", ref_perms(syms))
-print("got", fix(perms(syms)))
-
-ps = perms(syms)
-syms = [0, 1, 2, 4]
-print("exp", ref_perms(syms))
-print("got", fix(perms(syms)))
-
-
-syms = ""
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = "0"
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = "01"
-print("exp", ref_perms(syms))
-print("got", perms(syms))
-
-syms = "012"
-print("exp", ref_perms(syms))
-print("got", count_perms(syms))
-
-ps = perms(syms)
-syms = "0123"
-print("exp", ref_perms(syms))
-print("got", count_perms(syms))
+seq = [*'012']
+perm(seq, 0, len(seq))
