dc9697e72f97812126e843e1150ae7119833b9ae
John de la Garza <jdelagarza@cylance.com>
2023-08-03 14:23:42 -0700

    run: add

---
+++ markov/run.sh
@@ -0,0 +1,4 @@
+text='Show your flowcharts and conceal your tables and I will be mystified.  Show your tables and your flowcharts will be obvious.'
+
+
+javac Markov.java && echo $text | java Markov
